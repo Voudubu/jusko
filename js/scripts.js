f54110f90c007a1b5e7306f5d1cfe7790e31d135
Mateusz Jerzyk <tesla33a@gmail.com>
2016-12-18 01:25:01 +0100

    project update

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,28 @@ $(document).ready(function(){
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+
+    $(function() {
+        $(window).scroll( function(){
+        
+        
+            $('.fadeInBlock').each( function(i){
+                
+                var bottom_of_object = $(this).position().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                
+                /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+                bottom_of_window = bottom_of_window + 200;  
+            
+                if( bottom_of_window > bottom_of_object ){
+                    
+                    $(this).animate({'opacity':'1'},500);
+                        
+                }
+            }); 
+        
+        });
+    });
+
 });
